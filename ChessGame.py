d6135cfc2e4dddbb76c456a6a0ba68aca83a47f2
Explit <I.Explit.I@gmail.com>
2023-10-27 13:05:08 +0500

    change __init__ in ChessGame

--- ChessGame.py
+++ ChessGame.py
@@ -1,9 +1,20 @@
+from datetime import date
+
 class ChessGame:
     
+    players = []
+    motion = None
     history = []
     pieces = []
     
-    def __init__(self, board, player1, player2) -> None:
+    def __init__(
+        self, board, player1, player2,
+        event = '???', site = 'Explit\'s Chess Application', date = date(), 
+        ):
         self.board = board
-        self.player1 = player1
-        self.player2 = player2
+        self.players.append(player1)
+        self.players.append(player2)
+        self.event = event
+        self.site = site
+        self.date = date
+        
